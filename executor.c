7250411ccdb01dd442c334155a79c1f0441eabed
ochen1@stuy.edu <ochen1@stuy.edu>
2016-11-18 10:49:46 -0500

    k

---
+++ executor.c
@@ -0,0 +1,23 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(){
+  printf("enter a command ");
+  char s[50];
+  fgets(s,sizeof(s),stdin);
+  printf("%s\n",s);
+  char *a = s;
+  char *command[sizeof(s)+1];
+  int i;
+  for(i=0;command[i] = strsep(&a," ");i++);
+  printf("%d\n",i);
+  command[i+1]=0;
+  int j = 0;
+  while(command[j]){
+    printf("%s\n",command[j]);
+    j++;
+  }
+  execvp(command[0],command);
+}
